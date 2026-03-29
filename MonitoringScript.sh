b00c39c3f0465d3e3fe39fc0408dc4dc747d186d
root <root@ip-172-31-37-251.eu-north-1.compute.internal>
2026-03-29 15:33:19 +0000

    Bash script updated

--- MonitoringScript.sh
+++ MonitoringScript.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+##################
+#Author: NavyaSri
+#Date: 29/03/2026
+{ 
 
+echo " Date: $(date +"%D %H:%M:%S") "
+
+set -e #exit mode 
+set -o pipefail #pipefail handler 
+
+echo "Free space: "
+df -h
+
+echo "Available Mem: "
+free -g
+
+echo "No. of CPUs: "
+nproc
+
+echo "No. of processes running currently: "
+ps -ef | wc -l
+
+echo "CPU real time mem. usage: "
+top -b -n 1 
+} > file.txt 2>&1
