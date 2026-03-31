0d626ffb3f9a84c6a8bdc31c5c271ea6e5fa64a4
root <root@ip-172-31-37-251.eu-north-1.compute.internal>
2026-03-31 07:08:13 +0000

    Script is ready and successfully tested locally

--- MonitoringScript.sh
+++ MonitoringScript.sh
@@ -9,18 +9,28 @@ echo " Date: $(date +"%D %H:%M:%S") "
 set -e #exit mode 
 set -o pipefail #pipefail handler 
 
+echo "----------------------------------------------"
+
 echo "Free space: "
 df -h
+
+echo "----------------------------------------------"
 
 echo "Available Mem: "
 free -g
 
+echo "----------------------------------------------"
 echo "No. of CPUs: "
 nproc
 
+echo "----------------------------------------------"
 echo "No. of processes running currently: "
 ps -ef | wc -l
 
+echo "----------------------------------------------"
 echo "CPU real time mem. usage: "
-top -b -n 1 
-} > file.txt 2>&1
+top -b -n 1
+
+} > /root/proj/AWS-Resource-monitoring-script/file.txt 2>&1
+
+/usr/bin/mail -s "EC2 Monitoring Report - $(date +"%Y-%m-%d %H:%M")" navyasrigondela45@gmail.com < /root/proj/AWS-Resource-monitoring-script/file.txt
